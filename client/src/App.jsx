0e56b3a8603725263956ef5c80270d087b604c60
Richard Zhang <lapislacrima@gmail.com>
2016-11-29 01:30:33 +0000

    Chatbar entries now writes messages to the state.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,17 @@ class App extends Component {
     this.state = {
       chatMessages: [],
     }
+    this.newMessage = this.newMessage.bind(this);
+  }
+
+  newMessage(text, type="message"){
+    const id = this.state.chatMessages.length + 1;
+    this.setState = this.state.chatMessages.concat([{
+      id: id,
+      text: text,
+      type: type
+    }])
+    console.log(this.state);
   }
   render() {
     return (
@@ -16,10 +27,13 @@ class App extends Component {
           <h1> Placeholder text </h1>
         </div>
         <MessageList />
-        <ChatBar />
+        <ChatBar
+          newMessage = {this.newMessage}
+        />
       </div>
     )
   }
+
 }
 
 
--- client/src/ChatBar.jsx
+++ client/src/ChatBar.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 class ChatBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitHandler = this.submitHandler.bind(this);
+  }
+  submitHandler(e){
+    console.log("I am here");
+    this.props.newMessage(this.messageInput.value);
+    this.messageInput.value = '';
+  }
   render(){
     return (
-      <div>
-        <form>
-          <input></input>
+      <div id='entry_bar'>
+        <form onSubmit = {this.submitHandler}>
+          <input ref={el => this.messageInput = el} />
         </form>
       </div>
       )
